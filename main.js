38432c3189d13d6ad0677f74ab069fc7fa73a1bb
Ategmaria <ategmaria@gmail.com>
2025-04-10 01:10:12 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AMTecnologia from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AMTecnologia />
+  </React.StrictMode>
+);
